722542edafb64b0616625db0f3dc61b4ac147fa1
masudalakib <masudalakib@gmail.com>
2024-05-22 03:47:37 +0600

    operator

---
+++ 5conditional.py
@@ -0,0 +1,37 @@
+# in, not, not in, is, is not
+
+a= 1
+if a>5:
+
+    print('5 er besi')
+    print('koto besi ke jane')
+
+elif a>3:
+    print('boro boro olpo boro')
+
+elif a==2:
+    print('ekdom 2 er soman soman')
+
+else: 
+    print('coto coto number ti coto')
+
+    
+
+boss = False    
+
+# if boss is not True: 
+#     print('Boss nai , kam o nai')
+
+# else: 
+#     print('boss nai')
+
+
+
+if boss is True: 
+    print('Boss k jansci tel marte')
+
+else: 
+    print('time nai')
+
+
+
---
+++ Basic_Day1/4Operators.py
@@ -0,0 +1,19 @@
+# operatoon mane +,-,*,/,%,//
+
+num1= 45
+num2= 5
+print(num1 + num2)
+print(num1 - num2)
+print(num1 / num2)
+print(num1 * num2)
+print(10/3)
+print(10//3) #purno vag fol
+print(10%3) #reminder
+
+
+# > , <, >=, <=, ==, !==
+
+a=2
+b=3
+power = a**b 
+print(power)
