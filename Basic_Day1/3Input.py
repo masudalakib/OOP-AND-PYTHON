5307cdd6e03c524f82e40eb8c9c2c6c12d0bcc2a
masudalakib <masudalakib@gmail.com>
2024-05-22 01:54:06 +0600

    tk de

--- Basic_Day1/3Input.py
+++ Basic_Day1/3Input.py
@@ -1,2 +1,25 @@
-print("Now I need money")
-input('Give me some Money: 500')
+# print("Now I need money")
+# input('Give me some Money: 500')
+
+# money = input ('Give me some money: ')
+# print("Here is your money", money)
+
+
+first_money = input("kodom Ali, kisu tk de: ")
+second_money = input("sokina begon, kisu tk de: ")
+
+print('money i got from kodom', first_money, 'and from sokina', second_money)
+first_money_int =int(first_money)
+second_money_int= int(second_money)
+
+# total = first_money + second_money
+total = first_money_int + second_money_int
+print('total money i got', total)
+
+
+""" 
+Google search
+1. convert number to string: str
+2. convert decimal number: float
+3. python in vs float
+ """
