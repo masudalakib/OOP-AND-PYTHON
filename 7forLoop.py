de5db2814654d027856fbe5c06a71318dd1dbe7c
masudalakib <masudalakib@gmail.com>
2024-05-23 01:02:37 +0600

    loop

---
+++ 7forLoop.py
@@ -0,0 +1,36 @@
+#  for(int i=0; i<5; i++)
+#  {
+
+#  }
+
+
+
+
+# numbers = [5,10,15,20,25]
+# sum =0
+# for num in numbers:
+#     print(num)
+#     sum = sum + num
+
+#     if sum > 20:
+#         # print('bigger values')
+#         print('Bigger sum', sum)
+
+# print(sum)
+
+
+# text ='ay hai hoi hoi'
+# for char in text:
+#     print(char)
+
+
+
+for i in range(1,10):
+    print(i)
+
+
+
+friends =['kazi', 'nozrul', 'islam','thakur']
+for friend in friends:
+    print(friend)
+    
