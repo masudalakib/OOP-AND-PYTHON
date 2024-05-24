549b628e14025d7d6990c9577ad23f39b3b130bf
masudalakib <masudalakib@gmail.com>
2024-05-24 23:11:49 +0600

    function

---
+++ 2.Basic Concepts/function.py
@@ -0,0 +1,22 @@
+"""  """
+# define
+def double_it(num):
+    result = num *2
+    print(result)
+
+double_it(89)
+double_it(4)
+
+
+# sum korbo kono number
+def sum(num1, num2):
+    result = num1 + num2
+    return result
+
+total = sum (44,39)
+print('total value', total)
+
+
+
+final = double_it(total)
+print('final value', final)
